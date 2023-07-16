31f61bbdf29ea3b150cbfa05c5f76e37f86fee0f
Laura M <laurita_im@hotmail.com>
2023-07-16 15:06:00 -0700

    install bootstrap, added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+      <footer>
+        {/* <h3>Made by Laura Mendez©</h3> */}
+        <section> 
+          <ul> 
+          <p> Github  </p>
+            {/* <a href="https://github.com/lim204">
+              <li className="logo">
+                <VscGithubAlt />
+              </li>
+            </a> */}
+           <p> linkedIn </p>
+            {/* <a href="https://www.linkedin.com/in/laura-mendez-66185b244/">
+              <li className="logo">
+                <AiOutlineLinkedin />
+              </li>
+            </a> */}
+            <p>Instagram</p>
+  
+          </ul>
+        </section>
+      </footer>
+    );
+  }
