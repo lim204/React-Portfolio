598cd967dd5b24ce6677162ded595d2cb9b5411c
Laura M <laurita_im@hotmail.com>
2023-07-16 22:47:14 -0700

    creating footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,28 +3,31 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer>
-      {/* <h3>Made by Laura Mendez©</h3> */}
+
       <section>
         <ul class="nav justify-content-center">
-          <li class ="nav item">
+          <li class="nav item">
             <p> Github  </p>
             <a class="nav-link active" href="https://github.com/lim204">
-            {" "}
-           <img src="#" class="card-img" alt=".." />
+              {" "}
+              <img src="#" class="card-img" alt=".." />
             </a>
           </li>
 
-          <li class ="nav item">
-          <p> linkedIn </p>
+          <li class="nav item">
+            <p> linkedIn </p>
             <a class="nav-link active" href="https://www.linkedin.com/in/laura-mendez-66185b244/">
+              <img src="#" class="card-img" alt=".." />
             </a>
           </li>
-         
-         <li class ="nav item">
-         <p>Instagram</p>
-         <a class="nav-link active" href="#">
+
+          <li class="nav item">
+            <p>Instagram</p>
+            <a class="nav-link active" aria-current="page" href="#">
+              <img src="#" class="card-img" alt=".." />
             </a>
-         </li>
+
+          </li>
 
         </ul>
       </section>
